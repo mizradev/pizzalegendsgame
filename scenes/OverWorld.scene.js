f03459c0177a15873d7cd5f46b6e34413efbc70b
Jorge Aguilar <jorge_aguilar@wvi.org>
2024-01-01 18:08:47 -0600

    Refactor code and game object added

--- scenes/OverWorld.scene.js
+++ scenes/OverWorld.scene.js
@@ -13,40 +13,23 @@ class OverworldScene {
         
         image.src = "/img/maps/DemoLower.png";
 
-        const x = 5;
-        const y = 6;
+        
+        // Place some game Objects
+        const hero = new GameObject({
+            x: 5,
+            y: 6
+        });
 
-        const shadow = new Image();
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow,
-                0, // left cut
-                0, // top cut
-                32, // width of cut
-                32, // height of cut
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32
-            )
-        }
-        shadow.src = "/img/characters/shadow.png";
+        const npc1 = new GameObject({
+            x: 7,
+            y: 9,
+            src: "/img/characters/people/npc1.png"
+        });
 
-
-        const hero = new Image();
-        hero.onload = () => { this.ctx.drawImage(
-            hero, 
-            0, // left cut
-            0, // top cut
-            32, // width of cut
-            32, // height of cut
-            x * 16 - 8,
-            y * 16 - 18,
-            32,
-            32
-            ) 
-        }
-        hero.src = "/img/characters/people/hero.png";
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 200);
 
     }
 }
